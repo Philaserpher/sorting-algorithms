66a97bbf5cdc29b10df314fc483a21217cf0387a
Philaserpher <jpfgd20@gmail.com>
2022-02-02 22:03:13 +0000

    created insertion sort, added colours

--- SortingBar.py
+++ SortingBar.py
@@ -1,4 +1,6 @@
 # import pygame
+BLUE = (50, 72, 168)
+RED = (168, 50, 52)
 
 
 class SortingBar:
@@ -7,6 +9,13 @@ class SortingBar:
         self.height = height
         self.width = width
         self.pos = pos
+        self.colour = BLUE
 
     def get_height(self):
         return(self.height)
+
+    def make_red(self):
+        self.colour = RED
+
+    def make_blue(self):
+        self.colour = BLUE
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from SortingBar import SortingBar
 import random
 
+BLUE = (50, 72, 168)
+RED = (168, 50, 52)
 
 NUMBER_OF_BARS = 10
 
 
-def generate_random_bars(len, start=None, increment=None):
+def generate_random_bars(length, start=None, increment=None):
 
     if not start:
         start = 20
@@ -13,8 +15,24 @@ def generate_random_bars(len, start=None, increment=None):
         increment = 20
 
     bar_list = []
-    for i in range(0, len):
+    for i in range(length):
         bar_list.append(SortingBar(i*increment+start, 20, i))
+
+    return bar_list
+
+
+def insertion_sort(bar_list):
+    length = len(bar_list)
+    for i in range(length):
+        pointer = i-1
+        current = bar_list[i]
+        while (pointer >= 0 and
+               current.get_height() < bar_list[pointer].get_height()):
+
+            bar_list[pointer+1] = bar_list[pointer]
+            pointer -= 1
+        pointer += 1
+        bar_list[pointer] = current
 
     return bar_list
 
@@ -22,5 +40,6 @@ def generate_random_bars(len, start=None, increment=None):
 if __name__ == '__main__':
     bar_list = generate_random_bars(NUMBER_OF_BARS)
     random.shuffle(bar_list)
+    bar_list = insertion_sort(bar_list)
     for i in bar_list:
         print(i.get_height())
